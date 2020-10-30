197cfd2f54b0f00226016306ebbc98ef8da61473
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2020-10-31 01:29:13 +0300

    1

---
+++ libs/credentials.js
@@ -0,0 +1,11 @@
+var credentials = {};
+
+credentials["rocket"] = {
+    user:{
+        "X-User-Id":"a5DyvHKJyFCZDj8We",
+        "X-Auth-Token":"fNCa7G4jx2unEF-bq5FtBPRXKrLmuFIZ6s7VcP4WX0V"
+    }
+}
+
+
+module.exports = credentials;
