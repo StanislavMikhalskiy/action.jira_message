6ed21683a8d5ef14f56406680e3867ed6dda52d1
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2020-11-14 12:15:31 +0300

    появились промисы

--- libs/jira.js
+++ libs/jira.js
@@ -1,1 +1,119 @@
 const log = require('./log')(module);
+const fetch = require("node-fetch");
+const credentials = require('./credentials');
+
+// глобальные пемеренные
+var jiraURL = "https://jira.action-media.ru"
+var vGlobal = {
+    "jira": {
+        "viewIssue":jiraURL+"/browse/",
+        "getIssue":jiraURL+"/rest/api/2/issue/", // vGlobal.jira.getIssue
+        "postIssue":jiraURL+"/rest/api/2/issue/", // vGlobal.jira.postIssue
+        "postIssueBulk":jiraURL+"/rest/api/2/issue/bulk/", // vGlobal.jira.postIssueBulk
+        "postIssueLink":jiraURL+"/rest/api/2/issueLink/", // vGlobal.jira.postIssueLink
+        "fields":{
+            "epicLink":"customfield_10100", // vGlobal.jira.fields.epicLink
+            "epicName":"customfield_10102", // vGlobal.jira.fields.epicName
+            "businessCase":"customfield_11610", // vGlobal.jira.fields.businessCase
+            "team":"customfield_11601", // vGlobal.jira.fields.team
+            "components":{
+                "SS":"10014", // vGlobal.jira.fields.components.SS
+                "SEARCH":"10006",
+                "WARM":"10010"
+            },
+            "businessCases":{
+                "bigPicture":"11851" // vGlobal.jira.fields.businessCases.bigPicture
+            },
+            "teams":{
+                "SS":"11830", // vGlobal.jira.fields.teams.SS
+                "SEARCH":"11855",
+                "WARM":"11856"
+            },
+            "issueTypes":{
+                "bcklg":{
+                    "iniciative":"10903", // vGlobal.jira.fields.issueTypes.bcklg.iniciative
+                    "backendSub":"11001", // vGlobal.jira.fields.issueTypes.bcklg.backendSub
+                    "frontendSub":"11002", // vGlobal.jira.fields.issueTypes.bcklg.frontendSub
+                    "testSub":"11005" // vGlobal.jira.fields.issueTypes.bcklg.testSub
+                },
+                "dev":{
+                    "epic":"10000",
+                    "task":"10214" // vGlobal.jira.fields.issueTypes.dev.task
+                },
+                "support":{
+                    "dev":"10902" // vGlobal.jira.fields.issueTypes.support.dev
+                }
+            },
+            "issuePriorities":{
+                "support":{
+                    "high":"2" // vGlobal.jira.fields.issuePriorities.support.high
+                }
+            }
+        }
+    }
+}
+
+function getIssue(issueCode){
+    return new Promise(function(resolve,reject){
+        let url = new URL(vGlobal.jira.getIssue+issueCode);
+        fetch(url, {
+            method: 'get',
+            headers: {
+                "Content-type": "application/json; charset=utf-8",
+                "Authorization":credentials.jira.Authorization
+            }
+        }).then(response => {
+            if (response.status != "200") {
+                log.error(`Ошибка выполнения запроса для ${issueCode}, response.status = ${response.status} `);
+                response.json().then(function(data) {
+                    log.error(`${JSON.stringify(data)} `);
+                    reject(JSON.stringify(data));
+                });
+            } else {
+                response.json().then(function(data) {
+                    resolve(JSON.stringify(data));
+                })
+            }
+            }
+        )
+    })
+}
+
+function f1(){
+    let url = new URL(vGlobal.jira.getIssue+"SS-11886");
+    fetch(url, {
+        method: 'get',
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+            "Authorization":credentials.jira.Authorization
+        } // curl -u username:password -X GET -H "Content-Type: application/json" http://localhost:8080/rest/api/2/issue/createmeta
+    }).then(
+        function(response) {
+            log.info(`response.status = ${response.status} `);
+            if (response.status != "200") {
+                log.error(`Ошибка отправки сообщения, response.status = ${response.status} `);
+                response.json().then(function(data) {
+                    log.error(`${JSON.stringify(data)} `);
+                });
+            } else {
+                log.info(`Сообщение успешно отправлено`);
+                response.json().then(function(data) {
+                    log.info(`${JSON.stringify(data)}`);
+                })
+            }
+        }
+    )
+        .catch(function (error) {
+            log.error(`Ошибка отправки сообщения ${error}`);
+        });
+    return "a"
+}
+
+function f2(){
+    return "b"
+}
+
+
+module.exports.f1 = f1
+module.exports.f2 = f2
+module.exports.getIssue = getIssue
--- show_log_work.js
+++ show_log_work.js
@@ -1,4 +1,31 @@
 const log = require('./libs/log')(module);
-const module_jira = require('./libs/jira');
+const m_jira = require('./libs/jira');
 
 log.info("Go-go-go!");
+
+/*
+TODO
+
+ Входящие данные
+ - команда
+ - дата
+
+ Получаем все задачи, измененные за прошлый день по
+
+ https://jira.action-media.ru/rest/api/2/search?
+ fields=project,issuetype,resolution,summary,priority,status,parent,issuelinks,worklog,
+ customfield_10100&maxResults=1000&
+ jql=project%3D%2210900%22%20and%20worklogDate%20%3E%3D%20%222020-10-29%22%20and%20worklogDate%20%3C%20%222020-11-07%22&startAt=0&_=1605275998929
+
+* */
+
+var issuePromise =  m_jira.getIssue("SS-11886");
+issuePromise.then(
+    result => {
+        //log.info("ok");
+        //log.info(`${JSON.stringify(result)}`);
+        },
+    error => {
+        //log.info(`${JSON.stringify(error)}`);
+    }
+)
