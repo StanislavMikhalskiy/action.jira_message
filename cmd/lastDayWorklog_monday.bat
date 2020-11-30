34dba171c3b1498ef81f013356272f598463470c
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2020-11-30 17:15:27 +0300

    Добавили маркетинг

--- cmd/lastDayWorklog_monday.bat
+++ cmd/lastDayWorklog_monday.bat
@@ -1,3 +1,4 @@
 chcp 1251
 node E:\Project\action.jira_message\show_log_work.js --room=team-ss --rapidView=136 --day=3 --toAll=1
 node E:\Project\action.jira_message\show_log_work.js --room=team-webarm --rapidView=117 --day=3 --toAll=1
+node E:\Project\action.jira_message\show_log_work.js --room=marketing-platform --rapidView=507 --day=3 --toAll=1
